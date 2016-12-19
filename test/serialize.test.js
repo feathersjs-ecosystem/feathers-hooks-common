0faba66c6a1feedc74344f3a8769bbfbdb7939bc
bertho_l <kevin.bertho@gmail.com>
2016-12-20 00:11:07 +0100

    reverse serialize test changes

--- test/serialize.test.js
+++ test/serialize.test.js
@@ -47,44 +47,44 @@ describe('serialize', () => {
       _include: [ 'post' ],
       _elapsed: { post: 1, total: 1 },
       post:
-      { id: 1,
-        title: 'Post 1',
-        content: 'Lorem ipsum dolor sit amet 4',
-        author: 'as61389dadhga62343hads6712',
-        readers: [ 'as61389dadhga62343hads6712', '167asdf3689348sdad7312131s' ],
-        createdAt: 1480793101559,
-        _include: [ 'authorInfo', 'commentsInfo', 'readersInfo' ],
-        _elapsed: { authorInfo: 2, commentsInfo: 2, readersInfo: 2, total: 2 },
-        authorInfo: {
-          id: 'as61389dadhga62343hads6712',
-          name: 'Author 1',
-          email: 'author1@posties.com',
-          password: '2347wjkadhad8y7t2eeiudhd98eu2rygr',
-          age: 55
-        },
-        commentsInfo:
-        [
-          { title: 'Comment 1',
-            content: 'Lorem ipsum dolor sit amet 1',
-            postId: 1 },
-          { title: 'Comment 3',
-            content: 'Lorem ipsum dolor sit amet 3',
-            postId: 1 }
-        ],
-        readersInfo:
-        [
-          { id: 'as61389dadhga62343hads6712',
+        { id: 1,
+          title: 'Post 1',
+          content: 'Lorem ipsum dolor sit amet 4',
+          author: 'as61389dadhga62343hads6712',
+          readers: [ 'as61389dadhga62343hads6712', '167asdf3689348sdad7312131s' ],
+          createdAt: 1480793101559,
+          _include: [ 'authorInfo', 'commentsInfo', 'readersInfo' ],
+          _elapsed: { authorInfo: 2, commentsInfo: 2, readersInfo: 2, total: 2 },
+          authorInfo: {
+            id: 'as61389dadhga62343hads6712',
             name: 'Author 1',
             email: 'author1@posties.com',
             password: '2347wjkadhad8y7t2eeiudhd98eu2rygr',
-            age: 55 },
-          { id: '167asdf3689348sdad7312131s',
-            name: 'Author 2',
-            email: 'author2@posties.com',
-            password: '2347wjkadhad8y7t2eeiudhd98eu2rygr',
-            age: 16 }
-        ]
-      }
+            age: 55
+          },
+          commentsInfo:
+            [
+              { title: 'Comment 1',
+                content: 'Lorem ipsum dolor sit amet 1',
+                postId: 1 },
+              { title: 'Comment 3',
+                content: 'Lorem ipsum dolor sit amet 3',
+                postId: 1 }
+            ],
+          readersInfo:
+            [
+              { id: 'as61389dadhga62343hads6712',
+                name: 'Author 1',
+                email: 'author1@posties.com',
+                password: '2347wjkadhad8y7t2eeiudhd98eu2rygr',
+                age: 55 },
+              { id: '167asdf3689348sdad7312131s',
+                name: 'Author 2',
+                email: 'author2@posties.com',
+                password: '2347wjkadhad8y7t2eeiudhd98eu2rygr',
+                age: 16 }
+            ]
+        }
     };
 
     const hook1 = serialize(schema)(hook);
