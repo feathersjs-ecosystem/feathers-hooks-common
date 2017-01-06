9c363f3b2951b0b3c1f782a46e4b33b86446b329
John Szwaronek <johnsz9999@gmail.com>
2017-01-06 10:22:29 -0500

    Switching over to reorganized hook modules
    - Deleted original hook modules and tests
    - Next step is to break new modules into 1 file per hook

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
-export default Object.assign({},
-  require('./common'),
-  require('./bundled'),
-  require('./new'),
-  require('./populate'),
-  require('./dePopulate'),
-  require('./serialize')
+
+module.exports = Object.assign({},
+  require('./hooks/alter-data'),
+  require('./hooks/query-params'),
+  require('./hooks/populate'),
+  require('./hooks/legacyPopulate'),
+  require('./hooks/serialize'),
+  require('./hooks/database'),
+  require('./hooks/validate'),
+  require('./hooks/utilities'),
+  require('./hooks/conditionals'),
+  require('./hooks/utils'),
 );
