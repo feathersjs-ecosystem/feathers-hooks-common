4bb02416d5b0ae039b39f3ade1b4434ebd7d0f42
John Szwaronek <johnsz9999@gmail.com>
2017-01-08 09:10:46 -0500

    Handled services serialize.

--- src/services/database-split.js
+++ src/services/database-split.js
@@ -1,8 +1,11 @@
+// temporary
 
 import legacyPopulate from './legacy-populate';
+import serialize from './serialize';
 import softDelete from './soft-delete';
 
 export default {
+  serialize,
   legacyPopulate,
   softDelete
 };
