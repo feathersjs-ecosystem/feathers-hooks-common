ec1f0e02588d6a5deb4d32084bfbc76b39576dc0
John Szwaronek <johnsz9999@gmail.com>
2017-05-09 16:45:55 -0400

    Added sifter hook - filter result with mongodb queries

---
+++ src/services/sifter.js
@@ -0,0 +1,28 @@
+
+import errors from 'feathers-errors';
+
+import checkContext from './check-context';
+import getItems from './get-items';
+import replaceItems from './replace-items';
+
+// import sift from 'sift';
+// getSifter = hook => sift({ 'address.country': hook.data.country }
+export default function (getSifter) {
+  return hook => {
+    checkContext(hook, 'after', 'find', 'sifter');
+
+    if (typeof getSifter !== 'function') {
+      throw new errors.BadRequest(`The sifter param must be a function. (sifter)`);
+    }
+
+    const sifter = getSifter(hook);
+
+    if (typeof sifter !== 'function') {
+      throw new errors.BadRequest(`The result of calling the sifter param must be a function. (sifter)`);
+    }
+
+    replaceItems(hook, getItems(hook).filter(sifter));
+
+    return hook;
+  };
+}
