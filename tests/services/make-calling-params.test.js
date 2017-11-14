0d01f044c2f66a7ccabe3b8957fdd30590342915
John Szwaronek <johnsz9999@gmail.com>
2017-11-14 10:53:44 -0500

    Added fastJoin and related makeCallingParams plus tests

---
+++ tests/services/make-calling-params.test.js
@@ -0,0 +1,51 @@
+
+const { assert } = require('chai');
+const { makeCallingParams } = require('../../lib');
+
+let context;
+
+describe('service make-calling-params.test.js', () => {
+  beforeEach(() => {
+    context = { query: { aaa: 'bbb' }, foo: 'bar', baz: 'faz' };
+  });
+
+  it('does not retain current context', () => {
+    const res = makeCallingParams(context);
+    assert.deepEqual(res, { _populate: 'skip' });
+  });
+
+  it('sets query', () => {
+    const res = makeCallingParams(context, { a: 1 });
+    assert.deepEqual(res, { query: { a: 1 }, _populate: 'skip' });
+  });
+
+  it('sets include string', () => {
+    const res = makeCallingParams(context, null, 'foo');
+    assert.deepEqual(res, { foo: 'bar', _populate: 'skip' });
+  });
+
+  it('sets include array', () => {
+    const res = makeCallingParams(context, null, ['foo', 'baz']);
+    assert.deepEqual(res, { foo: 'bar', baz: 'faz', _populate: 'skip' });
+  });
+
+  it('sets include skip missing names', () => {
+    const res = makeCallingParams(context, null, ['foo', 'baz', 'x']);
+    assert.deepEqual(res, { foo: 'bar', baz: 'faz', _populate: 'skip' });
+  });
+
+  it('injects', () => {
+    const res = makeCallingParams(context, null, null, { aa: 2 });
+    assert.deepEqual(res, { aa: 2, _populate: 'skip' });
+  });
+
+  it('injects overwrites _populate', () => {
+    const res = makeCallingParams(context, null, null, { _populate: false });
+    assert.deepEqual(res, { _populate: false });
+  });
+
+  it('all work together', () => {
+    const res = makeCallingParams(context, { a: 1 }, ['foo', 'baz', 'x'], { aa: 2 });
+    assert.deepEqual(res, { query: { a: 1 }, foo: 'bar', baz: 'faz', aa: 2, _populate: 'skip' });
+  });
+});
