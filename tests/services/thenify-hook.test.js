41bead5f4a655a97e8b6d03068f96ea1c9bad0a9
John Szwaronek <johnsz9999@gmail.com>
2017-11-18 21:30:30 -0500

    Fixed linting

--- tests/services/thenify-hook.test.js
+++ tests/services/thenify-hook.test.js
@@ -56,29 +56,29 @@ describe('services thenifyHook', () => {
   });
 
   it('test using keep hook', () => {
-    const data = [{ name: 'John', job: 'dev', address: { city: 'Montreal', postal: 'H4T 2A1' }}];
+    const data = [{ name: 'John', job: 'dev', address: { city: 'Montreal', postal: 'H4T 2A1' } }];
 
     return Promise.resolve(data)
       .then(thenified(keep('name', 'address.city')))
       .then(result => {
-        assert.deepEqual(result, [{ name: 'John', address: { city: 'Montreal' }}]);
+        assert.deepEqual(result, [{ name: 'John', address: { city: 'Montreal' } }]);
       });
   });
 
   it('test using fastJoin hook', () => {
-    const paymentsRecords= [
+    const paymentsRecords = [
       { _id: 101, amount: 100, patientId: 1 },
       { _id: 102, amount: 105, patientId: 1 },
       { _id: 103, amount: 110, patientId: 1 },
       { _id: 104, amount: 115, patientId: 2 },
       { _id: 105, amount: 120, patientId: 3 },
-      { _id: 106, amount: 125, patientId: 3 },
+      { _id: 106, amount: 125, patientId: 3 }
     ];
 
     const patientsRecords = [
       { _id: 1, name: 'John' },
       { _id: 2, name: 'Marshall' },
-      { _id: 3, name: 'David' },
+      { _id: 3, name: 'David' }
     ];
 
     const paymentResolvers = {
@@ -86,8 +86,8 @@ describe('services thenifyHook', () => {
         patient: () => async payment => {
           payment.patient = (
             patientsRecords.filter(patient => patient._id === payment.patientId)
-          )[0]
-        },
+          )[0];
+        }
       }
     };
 
@@ -97,7 +97,7 @@ describe('services thenifyHook', () => {
       { _id: 103, amount: 110, patientId: 1, patient: { _id: 1, name: 'John' } },
       { _id: 104, amount: 115, patientId: 2, patient: { _id: 2, name: 'Marshall' } },
       { _id: 105, amount: 120, patientId: 3, patient: { _id: 3, name: 'David' } },
-      { _id: 106, amount: 125, patientId: 3, patient: { _id: 3, name: 'David' } } ]
+      { _id: 106, amount: 125, patientId: 3, patient: { _id: 3, name: 'David' } } ];
 
     return Promise.resolve(paymentsRecords)
       .then(thenified(fastJoin(paymentResolvers)))
