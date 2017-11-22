b8484c127a2fc48a90e69b1f6e0f3b941aa301cd
John Szwaronek <johnsz9999@gmail.com>
2017-11-22 15:41:18 -0500

    Added option to preventChanges to remove invalid prop names rather than throwing.

--- tests/services/prevent-changes.test.js
+++ tests/services/prevent-changes.test.js
@@ -1,11 +1,6 @@
 
-const {
-  assert
-} = require('chai');
-
-const {
-  preventChanges
-} = require('../../lib/services');
+const { assert } = require('chai');
+const { preventChanges } = require('../../lib/services');
 
 let hookBefore;
 
@@ -58,4 +53,76 @@ describe('services preventChanges', () => {
       assert.throw(() => preventChanges('name', 'a.c.d.e')(hookBefore));
     });
   });
+
+  describe('throws if first param is "true"', () => {
+    beforeEach(() => {
+      hookBefore = {
+        type: 'before',
+        method: 'patch',
+        params: { provider: 'rest' },
+        data: { first: 'John', last: 'Doe', a: { b: undefined, c: { d: { e: 1 } } } } };
+    });
+
+    it('does not throw if props not found', () => {
+      preventChanges(true, 'name', 'address')(hookBefore);
+      preventChanges(true, 'name.x', 'x.y.z')(hookBefore);
+    });
+
+    it('throw if props found', () => {
+      assert.throw(() => preventChanges(true, 'name', 'first')(hookBefore));
+      assert.throw(() => preventChanges(true, 'name', 'a')(hookBefore));
+      assert.throw(() => preventChanges(true, 'name', 'a.b')(hookBefore));
+      assert.throw(() => preventChanges(true, 'name', 'a.c')(hookBefore));
+      assert.throw(() => preventChanges(true, 'name', 'a.c.d.e')(hookBefore));
+    });
+  });
+
+  describe('deletes if first param is "false"', () => {
+    beforeEach(() => {
+      hookBefore = {
+        type: 'before',
+        method: 'patch',
+        params: { provider: 'rest' },
+        data: { first: 'John', last: 'Doe', a: { b: 'john', c: { d: { e: 1 } } } } };
+    });
+
+    it('does not delete if props not found', () => {
+      let context = preventChanges(false, 'name', 'address')(clone(hookBefore));
+      assert.deepEqual(context, hookBefore);
+
+      context = preventChanges(false, 'name.x', 'x.y.z')(clone(hookBefore));
+      assert.deepEqual(context, hookBefore);
+    });
+
+    it('deletes if props found', () => {
+      let context = preventChanges(false, 'name', 'first')(clone(hookBefore));
+      assert.deepEqual(context.data,
+        { last: 'Doe', a: { b: 'john', c: { d: { e: 1 } } } },
+      '1');
+
+      context = preventChanges(false, 'name', 'a')(clone(hookBefore));
+      assert.deepEqual(context.data,
+        { first: 'John', last: 'Doe' },
+        '2');
+
+      context = preventChanges(false, 'name', 'a.b')(clone(hookBefore));
+      assert.deepEqual(context.data,
+        { first: 'John', last: 'Doe', a: { c: { d: { e: 1 } } } },
+        '3');
+
+      context = preventChanges(false, 'name', 'a.c')(clone(hookBefore));
+      assert.deepEqual(context.data,
+        { first: 'John', last: 'Doe', a: { b: 'john' } },
+        '4');
+
+      context = preventChanges(false, 'name', 'a.c.d.e')(clone(hookBefore));
+      assert.deepEqual(context.data,
+        { first: 'John', last: 'Doe', a: { b: 'john', c: { d: {} } } },
+        '5');
+    });
+  });
 });
+
+function clone (obj) {
+  return JSON.parse(JSON.stringify(obj));
+}
