593776353db9386e93c38ba48b37c388202441b1
beeplin <beeplin@gmail.com>
2018-01-13 12:47:28 +0800

    Improve keep hook: allow passing in a function (item, context) => fields

--- tests/services/keep.test.js
+++ tests/services/keep.test.js
@@ -136,6 +136,98 @@ describe('services keep', () => {
       hooks.keep('first')(hook);
       assert.deepEqual(hook.data, { first: '' });
     });
+
+    it('updates hook before::create, with function', () => {
+      hooks.keep((item, context) => 'last')(hookBefore);
+      assert.deepEqual(hookBefore.data, { last: 'Doe' });
+    });
+
+    it('updates hook after::find with pagination, with function', () => {
+      hooks.keep((item, context) => 'first')(hookFindPaginate);
+      assert.deepEqual(hookFindPaginate.result.data, [
+        { first: 'John' },
+        { first: 'Jane' }
+      ]);
+    });
+
+    it('updates hook after::find with no pagination, with function', () => {
+      hooks.keep((item, context) => 'first')(hookFind);
+      assert.deepEqual(hookFind.result, [
+        { first: 'John' },
+        { first: 'Jane' }
+      ]);
+    });
+
+    it('updates hook after, with function', () => {
+      hooks.keep((item, context) => 'first')(hookAfter);
+      assert.deepEqual(hookAfter.result, { first: 'Jane' });
+    });
+
+    it('updates when called internally on server, with function', () => {
+      hookAfter.params.provider = '';
+      hooks.keep((item, context) => 'first')(hookAfter);
+      assert.deepEqual(hookAfter.result, { first: 'Jane' });
+    });
+
+    it('does not throw if field is missing, with function', () => {
+      const hook = {
+        type: 'before',
+        method: 'create',
+        params: { provider: 'rest' },
+        data: { first: 'John', last: 'Doe' } };
+      hooks.keep((item, context) => 'last', 'xx')(hook);
+      assert.deepEqual(hook.data, { last: 'Doe' });
+    });
+
+    it('keeps undefined values, with function', () => {
+      const hook = {
+        type: 'before',
+        method: 'create',
+        params: { provider: 'rest' },
+        data: { first: undefined, last: 'Doe' } };
+      hooks.keep((item, context) => 'first')(hook);
+      assert.deepEqual(hook.data, { first: undefined });
+    });
+
+    it('keeps null values, with function', () => {
+      const hook = {
+        type: 'before',
+        method: 'create',
+        params: { provider: 'rest' },
+        data: { first: null, last: 'Doe' } };
+      hooks.keep((item, context) => 'first')(hook);
+      assert.deepEqual(hook.data, { first: null });
+    });
+
+    it('keeps false values, with function', () => {
+      const hook = {
+        type: 'before',
+        method: 'create',
+        params: { provider: 'rest' },
+        data: { first: false, last: 'Doe' } };
+      hooks.keep((item, context) => 'first')(hook);
+      assert.deepEqual(hook.data, { first: false });
+    });
+
+    it('keeps 0 values, with function', () => {
+      const hook = {
+        type: 'before',
+        method: 'create',
+        params: { provider: 'rest' },
+        data: { first: 0, last: 'Doe' } };
+      hooks.keep((item, context) => 'first')(hook);
+      assert.deepEqual(hook.data, { first: 0 });
+    });
+
+    it('keeps empty string values, with function', () => {
+      const hook = {
+        type: 'before',
+        method: 'create',
+        params: { provider: 'rest' },
+        data: { first: '', last: 'Doe' } };
+      hooks.keep((item, context) => 'first')(hook);
+      assert.deepEqual(hook.data, { first: '' });
+    });
   });
 
   describe('handles dot notation', () => {
@@ -178,6 +270,39 @@ describe('services keep', () => {
 
     it('ignores bad or missing no dot path', () => {
       hooks.keep('xx')(hookBefore);
+      assert.deepEqual(hookBefore.data, {});
+    });
+
+    it('prop with no dots, with function', () => {
+      hooks.keep((item, context) => 'empl')(hookBefore);
+      assert.deepEqual(hookBefore.data,
+        { empl: { name: { first: 'John', last: 'Doe' }, status: 'AA' } }
+      );
+    });
+
+    it('prop with 1 dot, with function', () => {
+      hooks.keep((item, context) => ['empl.name', 'dept'])(hookBefore);
+      assert.deepEqual(hookBefore.data,
+        { empl: { name: { first: 'John', last: 'Doe' } }, dept: 'Acct' }
+      );
+    });
+
+    it('prop with 2 dots, with function', () => {
+      hooks.keep((item, context) => ['empl.name.last', 'empl.status', 'dept'])(hookBefore);
+      assert.deepEqual(hookBefore.data,
+        { empl: { name: { last: 'Doe' }, status: 'AA' }, dept: 'Acct' }
+      );
+    });
+
+    it('ignores bad or missing paths, with function', () => {
+      hooks.keep((item, context) => ['empl.name.first', 'empl.name.surname'])(hookBefore);
+      assert.deepEqual(hookBefore.data,
+        { empl: { name: { first: 'John' } } }
+      );
+    });
+
+    it('ignores bad or missing no dot path, with function', () => {
+      hooks.keep((item, context) => ['xx'])(hookBefore);
       assert.deepEqual(hookBefore.data, {});
     });
   });
