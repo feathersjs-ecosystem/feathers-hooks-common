ea564b7cc01c5c3c46bf30a3cafb74a43feb31ca
Aaron Burmeister <>
2018-03-09 13:57:29 +0100

    Applied linter suggestions: removed unnecessary escape character, removed unused function clone, spacing Added additional tests to increase coverage

--- tests/services/prevent-update-changes.test.js
+++ tests/services/prevent-update-changes.test.js
@@ -4,6 +4,13 @@ const { preventUpdateChanges } = require('../../lib/services');
 
 let hookBefore;
 
+const validate = (promise) => () => promise
+  .then(() => { throw new Error('Promise did not throw'); })
+  .catch((error) => {
+    if (error.message.match(/^Field [a-z.]+ may not be changed\. \(preventUpdateChanges\)$/)) return Promise.resolve();
+    else throw error;
+  });
+
 describe('services preventUpdateChanges', () => {
   describe('allowed for before update', () => {
     beforeEach(() => {
@@ -31,6 +38,47 @@ describe('services preventUpdateChanges', () => {
     });
   });
 
+  describe('uses default getter function', () => {
+    beforeEach(() => {
+      hookBefore = {
+        type: 'before',
+        method: 'update',
+        params: { provider: 'rest' },
+        data: { first: 'John', last: 'Doe', a: { b: undefined, c: { d: { e: 1 } } } } };
+    });
+
+    it('throw when doing a find with pagination', (done) => {
+      hookBefore.service = { find: () => ({ data: [{ first: 'Bill', last: 'Gates' }, { first: 'John', last: 'Doe' }] }) };
+
+      Promise.resolve()
+        .then(validate(preventUpdateChanges(undefined, 'first')(hookBefore)))
+        .then(() => done())
+        // test fails if any function call above didn't throw or an unexpected error occured
+        .catch((error) => done(error));
+    });
+
+    it('throw when doing a find without pagination', (done) => {
+      hookBefore.service = { find: () => [{ first: 'Bill', last: 'Gates' }, { first: 'John', last: 'Doe' }] };
+
+      Promise.resolve()
+        .then(validate(preventUpdateChanges(undefined, 'first')(hookBefore)))
+        .then(() => done())
+        // test fails if any function call above didn't throw or an unexpected error occured
+        .catch((error) => done(error));
+    });
+
+    it('throw when doing a get', (done) => {
+      hookBefore.id = 1;
+      hookBefore.service = { get: () => ({ first: 'Bill', last: 'Gates' }) };
+
+      Promise.resolve()
+        .then(validate(preventUpdateChanges(undefined, 'first')(hookBefore)))
+        .then(() => done())
+        // test fails if any function call above didn't throw or an unexpected error occured
+        .catch((error) => done(error));
+    });
+  });
+
   describe('throws on violation', () => {
     beforeEach(() => {
       hookBefore = {
@@ -42,24 +90,17 @@ describe('services preventUpdateChanges', () => {
 
     it('does not throw if no restricted fields changed', (done) => {
       Promise.resolve()
-      .then(() => preventUpdateChanges(() => ({ first: 'John', last: 'Doe' }), 'first')(hookBefore))
-      .then(() => preventUpdateChanges(() => ({ first: 'John', last: 'Jobs' }), 'first')(hookBefore))
-      .then(() => preventUpdateChanges(() => ({ first: 'John', last: 'Doe', a: { b: undefined, c: { d: { e: 1 } } } }), 'a')(hookBefore))
-      .then(() => preventUpdateChanges(() => ({ first: 'John', last: 'Doe', a: { b: undefined, c: { d: { e: 1 } } } }), 'a.b')(hookBefore))
-      .then(() => preventUpdateChanges(() => ({ first: 'John', last: 'Doe', a: { b: undefined, c: { d: { e: 1 } } } }), 'a.c')(hookBefore))
-      .then(() => preventUpdateChanges(() => ({ first: 'John', last: 'Doe', a: { b: undefined, c: { d: { e: 1 } } } }), 'a.c.d.e')(hookBefore))
-      .then(() => done())
-      .catch(error => done(error));
+        .then(() => preventUpdateChanges(() => ({ first: 'John', last: 'Doe' }), 'first')(hookBefore))
+        .then(() => preventUpdateChanges(() => ({ first: 'John', last: 'Jobs' }), 'first')(hookBefore))
+        .then(() => preventUpdateChanges(() => ({ first: 'John', last: 'Doe', a: { b: undefined, c: { d: { e: 1 } } } }), 'a')(hookBefore))
+        .then(() => preventUpdateChanges(() => ({ first: 'John', last: 'Doe', a: { b: undefined, c: { d: { e: 1 } } } }), 'a.b')(hookBefore))
+        .then(() => preventUpdateChanges(() => ({ first: 'John', last: 'Doe', a: { b: undefined, c: { d: { e: 1 } } } }), 'a.c')(hookBefore))
+        .then(() => preventUpdateChanges(() => ({ first: 'John', last: 'Doe', a: { b: undefined, c: { d: { e: 1 } } } }), 'a.c.d.e')(hookBefore))
+        .then(() => done())
+        .catch(error => done(error));
     });
 
     it('throw if restricted fields change', (done) => {
-      const validate = (promise) => () => promise
-        .then(() => { throw new Error('Promise did not throw'); })
-        .catch((error) => {
-          if (error.message.match(/^Field [a-z\.]+ may not be changed\. \(preventUpdateChanges\)$/)) return Promise.resolve();
-          else throw error;
-        });
-
       Promise.resolve()
         .then(validate(preventUpdateChanges(() => ({ first: 'Steve', last: 'Doe' }), 'first')(hookBefore)))
         .then(validate(preventUpdateChanges(() => ({ first: 'Steve', last: 'Jobs' }), 'first')(hookBefore)))
@@ -73,7 +114,3 @@ describe('services preventUpdateChanges', () => {
     });
   });
 });
-
-function clone (obj) {
-  return JSON.parse(JSON.stringify(obj));
-}
