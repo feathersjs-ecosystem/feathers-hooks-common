a6808b581051f08240e64f81a3007c047b84934c
David Luecke <daff@neyeon.com>
2019-11-12 17:22:56 -0800

    Remove dialable-phone-number

--- types/tests.ts
+++ types/tests.ts
@@ -14,7 +14,6 @@ import {
     debug,
     deleteByDot,
     dePopulate,
-    dialablePhoneNumber,
     disablePagination,
     disallow,
     discard,
@@ -64,7 +63,6 @@ import {
     when,
 } from 'feathers-hooks-common';
 import { parse } from 'graphql';
-import * as libphonenumberjs from 'libphonenumber-js';
 import ajv = require('ajv');
 
 const context1: HookContext = {
@@ -142,9 +140,6 @@ deleteByDot({}, 'abc.def');
 
 // $ExpectType Hook
 dePopulate();
-
-// $ExpectType Hook
-dialablePhoneNumber(libphonenumberjs);
 
 // $ExpectType Hook
 disablePagination();
