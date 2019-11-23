9011b2c9394cfc84667bd55db1d7bf1d08d26761
David Luecke <daff@neyeon.com>
2019-11-22 17:17:33 -0800

    New softDelete for Feathers 4 (#544)

--- tests/services/soft-delete.test.js
+++ tests/services/soft-delete.test.js
@@ -1,286 +1,245 @@
-
-const assert = require('chai').assert;
+const assert = require('assert');
 const feathers = require('@feathersjs/feathers');
 const memory = require('feathers-memory');
 const hooks = require('../../lib/services');
 
-const startId = 6;
-const storeInit = {
-  0: { name: 'Jane Doe', key: 'a', id: 0 },
-  1: { name: 'Jack Doe', key: 'a', id: 1 },
-  2: { name: 'Jack Doe', key: 'a', id: 2, deleted: true },
-  3: { name: 'Rick Doe', key: 'b', id: 3 },
-  4: { name: 'Dick Doe', key: 'b', id: 4 },
-  5: { name: 'Dick Doe', key: 'b', id: 5, deleted: true }
-};
+const initialUsers = [
+  { name: 'Jane Doe', key: 'a' },
+  { name: 'Jack Doe', key: 'a' },
+  { name: 'Jack Doe', key: 'a', deleted: true },
+  { name: 'Rick Doe', key: 'b' },
+  { name: 'Mick Doe', key: 'b' },
+  { name: 'Mick Doe', key: 'b', deleted: true }
+];
 
-let store;
-
-function services () {
-  const app = this;
-  app.configure(user);
-}
-
-function user () {
-  const app = this;
-  store = clone(storeInit);
-
-  class UsersService extends memory.Service {
-    constructor (...args) {
-      super(...args);
-      this.get_call_count = 0;
-    }
-
-    get (...args) {
-      this.get_call_count += 1;
-      return super.get(...args);
-    }
-  }
-
-  app.use('/users', new UsersService({
-    store,
-    startId,
-    multi: ['patch']
-  }));
-
-  app.service('users').hooks({
-    before: {
-      all: [
-        hook => {
-          if (hook.method === 'get') {
-            // getCallParams = clone(hook.params);
-          }
-        },
-        hooks.softDelete()
-      ]
-    }
-  });
-}
-
-describe.skip('services softDelete', () => {
-  let app;
-  let user;
+describe('services softDelete', () => {
+  let userService;
 
   beforeEach(() => {
-    app = feathers()
-      .configure(services);
+    const app = feathers()
+      .use('/users', memory({
+        multi: ['create', 'patch', 'remove']
+      }));
 
-    user = app.service('users');
+    userService = app.service('users');
+    userService.hooks({
+      before: {
+        all: [
+          hooks.softDelete()
+        ]
+      }
+    });
+
+    userService.create(initialUsers);
+  });
+
+  it('throws error on wrong app version', async () => {
+    const app = feathers()
+      .use('/users', memory());
+
+    app.service('users').hooks({
+      before: {
+        all: [
+          hooks.softDelete()
+        ]
+      }
+    });
+
+    app.version = '3.1.4';
+
+    await assert.rejects(() => app.service('users').find(), {
+      message: 'The softDelete hook requires Feathers 4.0.0 or later'
+    });
   });
 
   describe('find', () => {
-    it('find - does not return deleted items', done => {
-      user.find()
-        .then(data => {
-          assert.deepEqual(data, [store['0'], store['1'], store['3'], store['4']]);
-          done();
-        });
+    it('does not return deleted items', async () => {
+      const users = await userService.find();
+
+      assert.deepStrictEqual(users, [
+        { name: 'Jane Doe', key: 'a', id: 0 },
+        { name: 'Jack Doe', key: 'a', id: 1 },
+        { name: 'Rick Doe', key: 'b', id: 3 },
+        { name: 'Mick Doe', key: 'b', id: 4 }
+      ]);
+    });
+
+    it('returns everything with params.disableSoftdelete', async () => {
+      const users = await userService.find({
+        disableSoftDelete: true
+      });
+
+      assert.deepStrictEqual(users, [
+        { name: 'Jane Doe', key: 'a', id: 0 },
+        { name: 'Jack Doe', key: 'a', id: 1 },
+        { name: 'Jack Doe', key: 'a', deleted: true, id: 2 },
+        { name: 'Rick Doe', key: 'b', id: 3 },
+        { name: 'Mick Doe', key: 'b', id: 4 },
+        { name: 'Mick Doe', key: 'b', deleted: true, id: 5 }
+      ]);
     });
   });
 
   describe('get', () => {
-    it('returns an undeleted item', done => {
-      user.get(0)
-        .then(data => {
-          assert.deepEqual(data, storeInit['0']);
-          assert.equal(user.get_call_count, 1);
-          done();
-        });
+    it('returns an undeleted item', async () => {
+      const user = await userService.get(0);
+
+      assert.deepStrictEqual(user, {
+        name: 'Jane Doe', key: 'a', id: 0
+      });
     });
 
-    it('throws on deleted item', () => {
-      return user.get(2)
-        .then(() => assert.fail('Should never get here'))
-        .catch(error => {
-          assert.equal(error.name, 'NotFound');
-          assert.equal(error.message, 'No record found for id \'2\'');
-        });
+    it('throws on deleted item', async () => {
+      assert.rejects(async () => {
+        await userService.get(2);
+      }, {
+        name: 'NotFound',
+        message: 'No record found for id \'2\''
+      });
     });
 
-    it('throws on missing item', done => {
-      user.get(99)
-        .catch(() => {
-          assert.equal(user.get_call_count, 1);
-          done();
-        })
-        .then(data => {
-          assert.fail(true, false);
-          done();
-        });
-    });
-  });
+    it('returns deleted when params.disableSoftDelete is set', async () => {
+      const user = await userService.get(2, {
+        disableSoftDelete: true
+      });
 
-  describe('create', () => {
-    it('adds items', done => {
-      user.create({ name: 'John Doe', key: 'x' })
-        .then(data => {
-          const newUser = { name: 'John Doe', key: 'x', id: startId };
-          assert.deepEqual(data, newUser);
-          assert.deepEqual(store, Object.assign({}, store, { [startId]: newUser }));
-
-          done();
-        });
+      assert.deepStrictEqual(user, {
+        name: 'Jack Doe', key: 'a', deleted: true, id: 2
+      });
     });
 
-    it('adds items marked deleted', done => {
-      user.create({ name: 'John Doe', deleted: true })
-        .then(data => {
-          const newUser = { name: 'John Doe', deleted: true, id: startId };
-          assert.deepEqual(data, newUser);
-          assert.deepEqual(store, Object.assign({}, store, { [startId]: newUser }));
-
-          done();
-        });
+    it('throws on missing item', async () => {
+      await assert.rejects(async () => {
+        await userService.get(99);
+      }, {
+        name: 'NotFound',
+        message: 'No record found for id \'99\''
+      });
     });
   });
 
   describe('update, with id', () => {
-    it('updates an undeleted item', done => {
-      user.update(0, { y: 'y' })
-        .catch(err => console.log(err))
-        .then(data => {
-          assert.deepEqual(data, { y: 'y', id: 0 });
-          done();
-        });
+    it('updates an undeleted item', async () => {
+      const user = await userService.update(0, { y: 'y' });
+
+      assert.deepStrictEqual(user, { y: 'y', id: 0 });
     });
 
-    it('throws on deleted item', () => {
-      return user.update(2, { y: 'y' })
-        .then(() => assert.fail('Should never get here'))
-        .catch(error => {
-          assert.equal(error.message, 'No record found for id \'2\'');
-          assert.equal(error.name, 'NotFound');
-        });
-    });
-
-    it('throws on missing item', done => {
-      user.update(99, { y: 'y' })
-        .catch(() => {
-          done();
-        })
-        .then(data => {
-          assert.fail(true, false);
-          done();
-        });
+    it.skip('throws on deleted item', async () => {
+      await assert.rejects(async () => {
+        await userService.update(2, { y: 'y' });
+      }, {
+        name: 'NotFound',
+        message: 'No record found for id \'2\''
+      });
     });
   });
 
-  describe('patch, with id', () => {
-    it('patches an undeleted item', done => {
-      user.patch(0, { y: 'y' })
-        .then(data => {
-          assert.deepEqual(data, Object.assign({}, storeInit['0'], { y: 'y' }));
-          done();
-        });
+  describe('patch', () => {
+    it('patches an undeleted item', async () => {
+      const user = await userService.patch(0, { y: 'y' });
+
+      assert.deepStrictEqual(user, {
+        name: 'Jane Doe', key: 'a', id: 0, y: 'y'
+      });
     });
 
-    it('throws on deleted item', done => {
-      user.patch(2, { y: 'y' })
-        .catch(() => {
-          done();
-        })
-        .then(data => {
-          assert.fail(true, false);
-          done();
-        });
+    it('throws on deleted item', async () => {
+      await assert.rejects(() => userService.patch(2, { y: 'y' }), {
+        name: 'NotFound',
+        message: 'No record found for id \'2\''
+      });
     });
 
-    it('throws on missing item', done => {
-      user.patch(99, { y: 'y' })
-        .catch(() => {
-          done();
-        })
-        .then(data => {
-          assert.fail(true, false);
-          done();
-        });
-    });
-  });
+    it('multi updates on undeleted items', async () => {
+      const patched = await userService.patch(null, { x: 'x' });
 
-  describe('patch, without id', () => {
-    it('patches all nondeleted items if no filter', () => {
-      return user.patch(null, { x: 'x' })
-        .then(data => {
-          let expected = clone(
-            [storeInit['0'], storeInit['1'], storeInit['3'], storeInit['4']]);
-          expected.forEach(obj => { obj.x = 'x'; });
-          assert.deepEqual(data, expected);
-
-          expected = clone(storeInit);
-          [0, 1, 3, 4].forEach(i => { expected[i].x = 'x'; });
-          assert.deepEqual(store, expected);
-        });
-    });
-
-    it('patches filtered, nondeleted items', done => {
-      user.patch(null, { x: 'x' }, { query: { key: 'a' } })
-        .then(data => {
-          let expected = clone([storeInit['0'], storeInit['1']])
-            .map(obj => Object.assign({}, obj, { x: 'x' }));
-          assert.deepEqual(data, expected);
-
-          expected = clone(storeInit);
-          [0, 1].forEach(i => { expected[i].x = 'x'; });
-          assert.deepEqual(store, expected);
-          done();
-        });
+      assert.deepStrictEqual(patched, [
+        { name: 'Jane Doe', key: 'a', id: 0, x: 'x' },
+        { name: 'Jack Doe', key: 'a', id: 1, x: 'x' },
+        { name: 'Rick Doe', key: 'b', id: 3, x: 'x' },
+        { name: 'Mick Doe', key: 'b', id: 4, x: 'x' }
+      ]);
     });
   });
 
   describe('remove, with id', () => {
-    it('marks item as deleted', done => {
-      user.remove(0)
-        .then(data => {
-          assert.deepEqual(data, Object.assign({}, store['0'], { deleted: true }));
-          done();
-        });
+    it('marks item as deleted', async () => {
+      const user = await userService.remove(0);
+
+      assert.deepStrictEqual(user, {
+        name: 'Jane Doe', key: 'a', id: 0, deleted: true
+      });
+
+      await assert.rejects(() => userService.get(0), {
+        name: 'NotFound'
+      });
     });
 
-    it('throws if item already deleted', done => {
-      user.remove(2)
-        .catch(() => {
-          done();
-        })
-        .then(data => {
-          assert.fail(true, false);
-          done();
-        });
-    });
-
-    it('throws if item missing', done => {
-      user.remove(99)
-        .catch(() => {
-          done();
-        })
-        .then(data => {
-          assert.fail(true, false);
-          done();
-        });
+    it('throws if item already deleted', async () => {
+      await assert.rejects(() => userService.remove(2), {
+        name: 'NotFound'
+      });
     });
   });
 
   describe('remove, without id', () => {
-    it('marks filtered items as deleted', done => {
-      user.remove(null, { query: { key: 'a' } })
-        .then(data => {
-          assert.deepEqual(data, [
-            Object.assign({}, store['0'], { deleted: true }),
-            Object.assign({}, store['1'], { deleted: true })
-          ]);
-          done();
-        });
+    it('marks filtered items as deleted', async () => {
+      const query = { key: 'a' };
+      await userService.remove(null, { query });
+
+      const users = await userService.find({ query });
+
+      assert.strictEqual(users.length, 0);
     });
 
-    it('handles nothing found', done => {
-      user.remove(null, { query: { key: 'z' } })
-        .then(data => {
-          assert.deepEqual(data, []);
-          done();
-        });
+    it('handles nothing found', async () => {
+      const users = await userService.remove(null, { query: { key: 'z' } });
+
+      assert.strictEqual(users.length, 0);
+    });
+  });
+
+  describe('with customization: deletedAt', () => {
+    let peopleService;
+
+    beforeEach(() => {
+      const app = feathers()
+        .use('/people', memory({
+          multi: ['create', 'patch', 'remove']
+        }));
+
+      peopleService = app.service('people');
+      peopleService.hooks({
+        before: {
+          all: [
+            hooks.softDelete({
+              deletedQuery: async () => {
+                return { deletedAt: null };
+              },
+              removeData: async () => {
+                return { deletedAt: new Date() };
+              }
+            })
+          ],
+          create: [
+            context => {
+              context.data.deletedAt = null;
+            }
+          ]
+        }
+      });
+    });
+
+    it('works with setting deletedAt to date', async () => {
+      const user = await peopleService.create({ name: 'Jon Doe' });
+      const deletedUser = await peopleService.remove(user.id);
+
+      assert.ok(deletedUser.deletedAt !== null);
+
+      await assert.rejects(() => peopleService.get(user.id), {
+        name: 'NotFound'
+      });
     });
   });
 });
-
-function clone (obj) {
-  return JSON.parse(JSON.stringify(obj));
-}
