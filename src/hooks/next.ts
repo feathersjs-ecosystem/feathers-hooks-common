2f7b52761ee9fdeb1a1e426a93d1b00ea12926af
Ray Foss <ray@vblip.com>
2023-05-05 00:26:08 -0500

    remove unnecessary code

--- src/hooks/next.ts
+++ src/hooks/next.ts
@@ -9,7 +9,7 @@ async function* generateNext({ result, service, args = { query: {} } }) {
   yield result.data
   let pager = result
   while (pager.data.length > 0) {
-    args.query.$skip = (pager.limit + pager.skip) % pager.total
+    args.query.$skip = pager.limit + pager.skip
     pager = await service.find(args)
     yield pager.data
   }
