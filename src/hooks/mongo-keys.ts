c0aaff1f9fa3b55074906254fefc10c5f4d66452
fratzinger <22286818+fratzinger@users.noreply.github.com>
2024-07-21 22:42:34 +0200

    feat: neotraverse/legacy; v0.6.14 (#756)
    * Push
    
    * Remove this type for traverse
    
    * ci: update gh actions versions
    
    * ci: use node@22.5.1 explicitly
    
    ---------
    
    Co-authored-by: Puru Vijay <awesomepuruvj@gmail.com>

--- src/hooks/mongo-keys.ts
+++ src/hooks/mongo-keys.ts
@@ -1,5 +1,5 @@
 import type { HookContext } from '@feathersjs/feathers';
-import traverse from 'neotraverse';
+import traverse from 'neotraverse/legacy';
 import { checkContext } from '../utils/check-context';
 
 /**
@@ -26,7 +26,7 @@ export function mongoKeys<H extends HookContext = HookContext>(
     checkContext(context, 'before', null, 'mongoKeys');
     const query = context.params.query || {};
 
-    traverse(query).forEach(function (this: any, node: any) {
+    traverse(query).forEach(function (node: any) {
       const typeofNode = typeof node;
       const key = this.key;
       const path = this.path;
